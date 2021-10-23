86de7945519cf52fd73aa54e9890f3f812f0a77f
Suzanne Zomer <suzannezomer@gmail.com>
2021-10-23 17:54:00 +0200

    Mock HTTP request working
    first set up of the HTTP request to the mock API from postman working.

---
+++ MockedClientWeb/src/main/java/com/example/mockedclientweb/HttpRequest.java
@@ -0,0 +1,87 @@
+package com.example.mockedclientweb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpRequest {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "https://6967ccb6-8f01-46e3-b7cb-9567d5d8c139.mock.pstmn.io/devices";
+    private static final String POST_URL = "https://6967ccb6-8f01-46e3-b7cb-9567d5d8c139.mock.pstmn.io/devices/001?state=off";
+    private static final String POST_PARAMS = "userName=Suzanne";
+
+    public static void main(String[] args) throws IOException {
+
+        sendGET();
+        System.out.println("GET DONE");
+//        sendPOST();
+//        System.out.println("POST DONE");
+    }
+
+    private static void sendGET() throws IOException {
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+
+    }
+
+    private static void sendPOST() throws IOException {
+        URL obj = new URL(POST_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        // For POST only - START
+        con.setDoOutput(true);
+        OutputStream os = con.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+        // For POST only - END
+
+        int responseCode = con.getResponseCode();
+        System.out.println("POST Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST request not worked");
+        }
+    }
+
+}
