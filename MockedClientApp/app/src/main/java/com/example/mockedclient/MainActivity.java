4fe9b5cac674c7f2f93aa524d712e6461a7b2781
Fanny <fannysarasoderlund@gmail.com>
2021-10-29 15:39:48 +0200

    setup of preferences pane

--- MockedClientApp/app/src/main/java/com/example/mockedclient/MainActivity.java
+++ MockedClientApp/app/src/main/java/com/example/mockedclient/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mockedclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         TextView text1 = findViewById(R.id.textView);
         TextView text2 = findViewById(R.id.textView2);
+        Button settingsButton = findViewById(R.id.settingsButton);
 
+
+        settingsButton.setOnClickListener(v -> {
+            // opening a new intent to open settings activity.
+            Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(i);
+        });
     }
 }
