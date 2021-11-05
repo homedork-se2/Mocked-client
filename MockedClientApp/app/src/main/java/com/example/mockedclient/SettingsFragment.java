5f497a0d6ae002e655cbd06759f3965f77e7ef7b
Fanny <fannysarasoderlund@gmail.com>
2021-11-05 21:49:19 +0100

    Revert "haptic vibration setting"
    This reverts commit d35a49751613346aed630ae3e3409fd94026b23f.

--- MockedClientApp/app/src/main/java/com/example/mockedclient/SettingsFragment.java
+++ MockedClientApp/app/src/main/java/com/example/mockedclient/SettingsFragment.java
@@ -1,43 +1,21 @@
 package com.example.mockedclient;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Vibrator;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        SwitchPreferenceCompat vibrateSwitch = (SwitchPreferenceCompat) findPreference("vibration");
-
-        if (vibrateSwitch != null) {
-            vibrateSwitch.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference arg0, Object isVibrateOnObject) {
-                    boolean isVibrateOn = (Boolean) isVibrateOnObject;
-                    if (isVibrateOn) {
-                        Vibrator v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                        v.vibrate(400);
-                        Toast.makeText(getContext(), "VIBRATING", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(getContext(), "NOT VIBRATING", Toast.LENGTH_SHORT).show();
-                    }
-                    return true;
-                }
-            });
-        }
+        addPreferencesFromResource(R.xml.root_preferences);
     }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
     }
 }
